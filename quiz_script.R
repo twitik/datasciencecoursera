c133891130161caf3b7eabb69d303899badbb92c
Alexander <twitik@gmail.com>
2015-01-16 11:08:51 +0300

    making NA matrix

--- quiz_script.R
+++ quiz_script.R
@@ -1,1 +1,14 @@
-for (i in 1:153) print(m[i,1])
+for (i in 1:153) 
+{
+  if (m[i,1]>31) print(m[1,1]) 
+  
+}
+
+i <- 5
+if (m[i,1]>31 & m[i,4]>10) { 
+  print(m[i,1])
+  print(m[i,4])
+}
+
+print(m[5,1])
+print(m[5,4])
